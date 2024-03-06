ca14bb8d56ace37f0f61bdf79e1d633164072a0b
Dhruv Sharma <dhruvamsh@gmail.com>
2024-03-06 15:19:35 +0530

    Initial commit

---
+++ server/app.ts
@@ -0,0 +1,45 @@
+import express, { NextFunction, Request, Response } from "express";
+export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+require("dotenv").config();
+import { ErrorMiddleware } from "./middleware/error";
+import userRouter from "./routes/user.route";
+
+// body Parse
+app.use(express.json({ limit: "50mb" }));
+
+// cookie parser
+app.use(cookieParser());
+
+// cors
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+// testing api
+
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working",
+  });
+});
+
+// routes
+
+app.use("/api/v1",userRouter); 
+
+
+
+// unknown route
+
+app.use("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use(ErrorMiddleware);
---
+++ server/middleware/error.ts
@@ -0,0 +1,44 @@
+import { NextFunction, Request, Response } from "express";
+import ErrorHandler from "../utils/ErrorHanlder";
+
+export const ErrorMiddleware = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+
+  // wrong MongoDB Id
+
+  if (err.name === "CastError") {
+    const message = `Resource not found, Invalid: ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // Duplicate key error
+
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //   wrong JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json web token is invalid, try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //   JWT expired
+
+  if (err.name === "TokenExpiredError") {
+    const message = `Josn web token is expired, try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
