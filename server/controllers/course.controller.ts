f7a0af0ea6ad255eb6c52acfd611b92b23e81d2b
Dhruv Sharma <dhruvamsh@gmail.com>
2024-03-11 16:17:14 +0530

    Implemented Q&A feature for course module, enhancing user engagement! 💬🚀
    🚀 Added question and replies functionality for course interaction. Users can now engage in discussions, ask questions, and receive answers within the course platform, fostering a collaborative learning environment. This feature enhances the learning experience by facilitating communication and knowledge sharing among participants. 💬📚

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -6,6 +6,10 @@ import { createCourse } from "../services/course.service";
 import CourseModel from "../models/course.model";
 import { redis } from "../utils/redis";
 import { stringify } from "querystring";
+import mongoose from "mongoose";
+import path from "path";
+import ejs from "ejs";
+import sendMail from "../utils/sendMail";
 
 // upload course
 
@@ -162,8 +166,137 @@ export const getCourseByUser = CatchAsyncError(
   }
 );
 
-
-
 // add question in course
 
-interface 
+interface IAddQuestionData {
+  question: string;
+  courseId: string;
+  contentId: string;
+}
+
+export const addQuestion = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { question, courseId, contentId }: IAddQuestionData = req.body;
+      const course = await CourseModel.findById(courseId);
+
+      if (!mongoose.Types.ObjectId.isValid(contentId)) {
+        return next(new ErrorHandler("Invalid content id", 400));
+      }
+
+      const courseContent = course?.courseData?.find((itme: any) =>
+        itme._id.equals(contentId)
+      );
+
+      if (!courseContent) {
+        return next(new ErrorHandler("Invalid content id", 400));
+      }
+
+      // create a new question Object
+
+      const newQuestion: any = {
+        user: req.user,
+        question,
+        questionReplies: [],
+      };
+
+      // add this question to our course content
+
+      courseContent.questions.push(newQuestion);
+
+      // save the updated course
+      await course?.save();
+
+      res.status(200).json({
+        success: true,
+        course,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+// add answer in course question
+interface IAddAnswerData {
+  answer: string;
+  courseId: string;
+  contentId: string;
+  questionId: string;
+}
+
+export const addAnswer = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { answer, courseId, contentId, questionId }: IAddAnswerData =
+        req.body;
+      const course = await CourseModel.findById(courseId);
+
+      if (!mongoose.Types.ObjectId.isValid(contentId)) {
+        return next(new ErrorHandler("Invalid content id", 400));
+      }
+
+      const courseContent = course?.courseData?.find((itme: any) =>
+        itme._id.equals(contentId)
+      );
+
+      if (!courseContent) {
+        return next(new ErrorHandler("Invalid content id", 400));
+      }
+
+      const question = courseContent?.questions.find((item: any) =>
+        item._id.equals(questionId)
+      );
+
+      if (!question) {
+        return next(new ErrorHandler("Inavlid question id", 400));
+      }
+
+      // create a new question Object
+
+      const newAnswer: any = {
+        user: req.user,
+        answer,
+      };
+
+      // add this question to our course content
+
+      question.questionReplies.push(newAnswer);
+
+      // save the updated course
+      await course?.save();
+
+      if (req.user?._id === question.user._id) {
+        // create a notification
+      } else {
+        const data = {
+          name: question.user.name,
+          title: courseContent.title,
+        };
+
+        const html = await ejs.renderFile(
+          path.join(__dirname, "../mails/question-reply.ejs"),
+          data
+        );
+
+        try {
+          await sendMail({
+            email: question.user.email,
+            subject: "Question Reply",
+            template: "question-reply.ejs",
+            data,
+          });
+        } catch (error: any) {
+          return next(new ErrorHandler(error.message, 500));
+        }
+      }
+
+      res.status(200).json({
+        success: true,
+        course,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -12,6 +12,7 @@ import {
   updateUserInfo,
 } from "../controllers/user.controller";
 import { authorizeRole, isAuthenticated } from "../middleware/auth";
+import { addAnswer, addQuestion } from "../controllers/course.controller";
 const userRouter = express.Router();
 
 userRouter.post("/registration", registrationUser);
@@ -24,5 +25,7 @@ userRouter.post("/socailAuth", socialAuth);
 userRouter.put("/update-user-info", isAuthenticated, updateUserInfo);
 userRouter.put("/update-user-password", isAuthenticated, updatePassword);
 userRouter.put("/update-user-avatar", isAuthenticated, updateProfilePicture);
+userRouter.put("/add-question", isAuthenticated, addQuestion);
+userRouter.put("/add-anser", isAuthenticated, addAnswer);
 
 export default userRouter;
